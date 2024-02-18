aa4d3077be857d170d945a62af9174f79295ceb8
RitikTailor15 <ritiktailor15@gmail.com>
2024-02-18 08:02:40 +0530

    Test onChange event on input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState("");
   return (
     <div className="App">
       <p>Test First React app</p>
-      <input
+      {/* <input
         type="text"
         placeholder="Enter user name"
         name="username"
         id="userId"
         value="Ritik Tailor"
         readOnly
-      />
+      /> */}
+      <br />
       <img
         title="Car Image"
         src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?cs=srgb&dl=pexels-mike-bird-170811.jpg&fm=jpg"
@@ -19,6 +22,12 @@ function App() {
           width: "500px",
         }}
         alt="img"
+      />
+      <br />
+      <input
+        type="text"
+        value={data}
+        onChange={(e) => setData(e.target.value)}
       />
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("Test First React App", () => {
@@ -9,17 +9,17 @@ test("Test First React App", () => {
   expect(title).toBeInTheDocument();
 });
 
-test("Test Input Box", () => {
-  render(<App />);
-  const inputEl = screen.getByRole("textbox");
-  const placeholder = screen.getByPlaceholderText("Enter user name");
-  expect(inputEl).toBeInTheDocument();
-  expect(placeholder).toBeInTheDocument();
-  expect(inputEl).toHaveAttribute("name", "username");
-  expect(inputEl).toHaveAttribute("id", "userId");
-  expect(inputEl).toHaveAttribute("value", "Ritik Tailor");
-  expect(inputEl).toHaveAttribute("type", "text");
-});
+// test("Test Input Box", () => {
+//   render(<App />);
+//   const inputEl = screen.getByRole("textbox");
+//   const placeholder = screen.getByPlaceholderText("Enter user name");
+//   expect(inputEl).toBeInTheDocument();
+//   expect(placeholder).toBeInTheDocument();
+//   expect(inputEl).toHaveAttribute("name", "username");
+//   expect(inputEl).toHaveAttribute("id", "userId");
+//   expect(inputEl).toHaveAttribute("value", "Ritik Tailor");
+//   expect(inputEl).toHaveAttribute("type", "text");
+// });
 
 describe("UI Test Group", () => {
   test("Group First Test", () => {
@@ -34,3 +34,13 @@ describe("UI Test Group", () => {
   as describe.skip it will skip the whole group
   and describe.only will run that group and skip the others 
  */
+
+// Test onChange event
+
+test("Test onChange event on input box", () => {
+  render(<App />);
+  const inputEl = screen.getByRole("textbox");
+  expect(inputEl).toBeInTheDocument();
+  fireEvent.change(inputEl, { target: { value: "Ritik Tailor" } });
+  expect(inputEl.value).toBe("Ritik Tailor");
+});
